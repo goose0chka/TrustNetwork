ba597ca951f5dd114ef7e65b10a35a7add7f3646
Ihor Lupyna <62049009+goose0chka@users.noreply.github.com>
2022-09-29 18:15:21 +0300

    added base model

---
+++ TrustNetwork.DAL/Model/Person.cs
@@ -0,0 +1,7 @@
+﻿namespace TrustNetwork.DAL.Model;
+public class Person
+{
+    public string Id { get; set; } = string.Empty;
+    public ICollection<string> Topics { get; set; } = new List<string>();
+    public ICollection<Relation> Realtions { get; set; } = new List<Relation>();
+}
---
+++ TrustNetwork.DAL/Model/Relation.cs
@@ -0,0 +1,7 @@
+﻿namespace TrustNetwork.DAL.Model;
+public class Relation
+{
+    public int PersonId { get; set; }
+    public int ContactId { get; set; }
+    public int TrustLevel { get; set; }
+}
